44659f8aa855e120d6b9e5a756392291f3ccdcd6
Michael Krausse <michael.krausse@directbox.com>
2021-07-03 09:36:18 +0200

    initial

---
+++ src/main/java/de/ehmkah/projects/springexamlples/SpringexamlplesApplication.java
@@ -0,0 +1,13 @@
+package de.ehmkah.projects.springexamlples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringexamlplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringexamlplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/ehmkah/projects/springexamlples/SpringexamlplesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ehmkah.projects.springexamlples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringexamlplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
